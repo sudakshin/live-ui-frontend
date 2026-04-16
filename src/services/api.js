c252d3a524094b530736b9df644775421e34bd30
Sudakshin Kumar <sudakshin9@gmail.com>
2026-04-16 23:29:15 +0530

    cors fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios"
-
-const API_URL = "/api/live"
+const API_URL = "https://live.sudaksh.in/live"
 
 export async function fetchStreams() {
   const res = await axios.get(API_URL)
